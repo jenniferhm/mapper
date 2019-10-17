55f9b07ec14495400d5eabcd1f053fd2da83b258
Jennifer Ma <jenniferma93@gmail.com>
2019-10-17 11:59:02 -0700

    created sample data for pre-loading markers on map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper, InfoWindow, Marker} from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker, google} from 'google-maps-react';
 import uuid from "uuid/v4"
 
 const mapStyles = {
@@ -22,6 +22,7 @@ export class MapContainer extends Component {
 
   onMapClick = (props, location, e) => {
     let newMarker = <Marker key={uuid()} onClick={this.onMarkerClick} position={e.latLng}/>
+    console.log("coordinates", `${newMarker.props.position.lat()}, ${newMarker.props.position.lng()}`)
       this.setState({
         showingInfoWindow: false,
         activeMarker: null,
@@ -29,7 +30,7 @@ export class MapContainer extends Component {
       })
     }
 
-  onMarkerClick = (props, marker, e) => {
+    onMarkerClick = (props, marker, e) => {
     return this.setState({
       selectedPlace: props,
       activeMarker: marker,
@@ -56,8 +57,8 @@ export class MapContainer extends Component {
         zoom={14}
         style={mapStyles}
         initialCenter={{
-          lat: -1.2884,
-          lng: 36.8233
+          lat: 37.7606574,
+          lng: -122.4619144
         }}
         onClick={this.onMapClick}
         >
